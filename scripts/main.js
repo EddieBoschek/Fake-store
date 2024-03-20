82c7cf53bbbd61a295dac5d96dd4c8d1f1e40844
”Eddie <eddie.boschek@gmail.com>
2024-03-20 14:09:44 +0100

    Validation and form connected. Valid form redirects to confirmation page.

--- scripts/main.js
+++ scripts/main.js
@@ -75,3 +75,81 @@ document.addEventListener('DOMContentLoaded', () => {
 
   products.getProducts().then((products) => ui.displayProducts(products));
 });
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  let firstName = document.getElementById("fname").value;
+  let lastName = document.getElementById("lname").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let address = document.getElementById("address").value;
+  let zip = document.getElementById("zip").value;
+  let city = document.getElementById("city").value;
+
+  resetErrors();
+
+  let isValid = true;
+  if (!validInputSize(firstName)) {
+      document.getElementById("fnameError").innerText = "Input must be between 2 and 50 characters long.";
+      isValid = false;
+  }
+  if (!validInputSize(lastName)) {
+      document.getElementById("lnameError").innerText = "Input must be between 2 and 50 characters long.";
+      isValid = false;
+  }
+  if (!validEmail(email)) {
+      document.getElementById("emailError").innerText = "Please enter a valid email.";
+      isValid = false;
+  }
+  if (!validPhoneNumber(phone)) {
+    document.getElementById("phoneError").innerText = "Please enter a valid phone number.";
+    isValid = false;
+  }
+  if (!validInputSize(address)) {
+    document.getElementById("addressError").innerText = "Input must be between 2 and 50 characters long.";
+    isValid = false;
+  }
+  if (!validZip(zip)) {
+    document.getElementById("zipError").innerText = "Please enter a valid ZIP code.";
+    isValid = false;
+  }
+  if (!validInputSize(city)) {
+    document.getElementById("cityError").innerText = "Input must be between 2 and 50 characters long.";
+    isValid = false;
+  }
+  if (isValid) {
+    localStorage.setItem('first-name', firstName)
+    localStorage.setItem('last-name', lastName)
+    localStorage.setItem('email', email)
+    localStorage.setItem('phone', phone)
+    localStorage.setItem('address', address)
+    localStorage.setItem('zip', zip)
+    localStorage.setItem('city', city)
+
+    window.location.href = "confirmation.html";
+  }
+});
+
+function validInputSize(input) {
+  return /^.{2,50}$/.test(input);
+}
+function validEmail(input) {
+  return /^(?=.{1,50}$)[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+}
+function validZip(input) {
+  return /^\d{3}\s*\d{2}$/.test(input);
+}
+function validPhoneNumber(input) {
+  return /^07[0236]-?\d{7}$/.test(input);
+}
+
+function resetErrors() {
+  document.getElementById("fnameError").innerText = "";
+  document.getElementById("lnameError").innerText = "";
+  document.getElementById("emailError").innerText = "";
+  document.getElementById("phoneError").innerText = "";
+  document.getElementById("addressError").innerText = "";
+  document.getElementById("zipError").innerText = "";
+  document.getElementById("cityError").innerText = "";
+}
